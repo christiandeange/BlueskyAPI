17291a081c7722fb7c46eaa04bed2665c2de716e
christiandeange <3208823+christiandeange@users.noreply.github.com>
2025-05-01 07:43:01 +0000

    Releasing v0.3.3.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://repo1.maven.org/maven2/sh/christian/ozone/bluesky-kmmbridge/0.3.2/bluesky-kmmbridge-0.3.2.zip"
-let remoteKotlinChecksum = "c61883bedcf01a7f988e0f3cf681334162d6d60e0ea27613e851add168e7c515"
+let remoteKotlinUrl = "https://repo1.maven.org/maven2/sh/christian/ozone/bluesky-kmmbridge/0.3.3/bluesky-kmmbridge-0.3.3.zip"
+let remoteKotlinChecksum = "66d8e85d24bf85f1cd557e6246c5c29975ccfa9f75a1951d285b447b12fc945f"
 let packageName = "BlueskyAPI"
 
 let package = Package(
